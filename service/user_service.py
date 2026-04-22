1f72f1ee65fc68fd98c8ef1e93ce1789d2ef7cda
Hua <13030119817@163.com>
2026-04-22 16:50:28 +0800

    bug

--- service/user_service.py
+++ service/user_service.py
@@ -114,7 +114,7 @@ class UserService:
                 'account': user.account,
                 'nickname': user.nickname,
                 'session_id': session_id,
-                "avatar": user.avatar
+                "avatar": user.avatar if user.avatar else "",
             },
             expires_delta=access_token_expires,
         )
